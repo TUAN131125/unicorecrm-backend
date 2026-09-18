51e360a06139fc0cb9d024a6ee8f390d9cb36b4c
NGUYEN MINH TUAN <163620119+TUAN131125@users.noreply.github.com>
2026-09-18 11:22:39 +0700

    Register Communications persistence and migration

--- src/UnicoreCRM.Communications/CommunicationsModule.cs
+++ src/UnicoreCRM.Communications/CommunicationsModule.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using UnicoreCRM.BuildingBlocks;
+using UnicoreCRM.Communications.Application.Common;
 using UnicoreCRM.Communications.Infrastructure.Persistence;
 
 namespace UnicoreCRM.Communications;
@@ -17,6 +19,16 @@ public static class CommunicationsModule
             options.UseSqlServer(
                 connectionString,
                 sql => sql.MigrationsHistoryTable("__EFMigrationsHistory", "communications")));
+
+        services.AddScoped<ICommunicationsPersistence, EfCommunicationsPersistence>();
+        services.AddDevelopmentSchemaMigration(
+            "communications",
+            (provider, cancellationToken) =>
+                provider.GetRequiredService<CommunicationsDbContext>()
+                    .Database.MigrateAsync(cancellationToken));
+
+        services.AddScoped<Application.ListSenderProfiles.Handler>();
+        services.AddScoped<Application.ListEmailConnections.Handler>();
 
         return services;
     }
